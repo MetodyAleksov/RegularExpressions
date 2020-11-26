0421d63d9a0f0141dabca80861409eb437050dd6
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-11-26 15:46:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            Regex regex = new Regex(pattern);
+            var names = regex.Matches(text);
+
+            Console.WriteLine(string.Join(" ", names));
+        }
+    }
+}
